537a141828f1524c9da35e43b3d248e8fe1b2565
Joan TK <149820143+JJoan02@users.noreply.github.com>
2024-07-05 20:41:07 -0500

    Update handler.js

--- handler.js
+++ handler.js
@@ -450,38 +450,31 @@ function pickRandom(list) { return list[Math.floor(Math.random() * list.length)]
  * @param {import('@adiwajshing/baileys').BaileysEventMap<unknown>['group-participants.update']} groupsUpdate 
  */
 export async function participantsUpdate({ id, participants, action }) {
-  if (opts['self'])
-    return;
-  if (this.isInit)
-    return;
-  if (global.db.data == null)
-    await loadDatabase();
-  let chat = global.db.data.chats[id] || {};
-  let botTt = global.db.data.settings[conn.user.jid] || {};
-  let text = '';
-  switch (action) {
-    case 'add':
-      if (chat.welcome) {
-        let groupMetadata = await this.groupMetadata(id) || (conn.chats[id] || {}).metadata;
-        for (let user of participants) {
-          let pp = gataImg.getRandom();
-          try {
-            pp = await this.profilePictureUrl(user, 'image');
-          } catch (e) {
-          } finally {
-            let apii = await this.getFile(pp);
-            const botTt2 = groupMetadata.participants.find(u => this.decodeJid(u.id) == this.user.jid) || {};
-            const isBotAdminNn = botTt2?.admin === "admin" || false;
-            text = (chat.sWelcome || this.welcome || conn.welcome || 'Welcome, @user!')
-              .replace('@subject', await this.getName(id))
-              .replace('@desc', groupMetadata.desc?.toString() || '🔰 𝗦𝘂𝗽𝗲𝗿 𝗔𝗱𝗺𝗶𝗻-𝗧𝗞 🔰')
-              .replace('@user', '@' + user.split('@')[0]);
-          }
-        }
-      }
-      break;
-  }
-}
+if (opts['self'])
+return
+if (this.isInit)
+return
+if (global.db.data == null)
+await loadDatabase()
+let chat = global.db.data.chats[id] || {}
+let botTt = global.db.data.settings[conn.user.jid] || {}
+let text = ''
+switch (action) {
+case 'add':
+case 'remove':
+if (chat.welcome) {
+let groupMetadata = await this.groupMetadata(id) || (conn.chats[id] || {}).metadata
+for (let user of participants) {
+let pp = gataImg.getRandom()
+try {
+pp = await this.profilePictureUrl(user, 'image')
+} catch (e) {
+} finally {
+let apii = await this.getFile(pp)                                      
+const botTt2 = groupMetadata.participants.find(u => this.decodeJid(u.id) == this.user.jid) || {} 
+const isBotAdminNn = botTt2?.admin === "admin" || false
+text = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || 'Welcome, @user!').replace('@subject', await this.getName(id)).replace('@desc', groupMetadata.desc?.toString() || '🔰 𝗦𝘂𝗽𝗲𝗿 𝗔𝗱𝗺𝗶𝗻-𝗧𝗞 🔰') :
+(chat.sBye || this.bye || conn.bye || 'Bye, @user!')).replace('@user', '@' + user.split('@')[0])
 
 if (chat.antifake && botTt.restrict && isBotAdminNn && action === 'add') {
 const numerosPermitidos = ["1", "2", "4", "6", "7", "8", "9"] //PUEDES EDITAR LOS USUARIOS QUE SE ELIMINARÁN SI EMPIEZA POR CUALQUIER DE ESOS NÚMEROS	
