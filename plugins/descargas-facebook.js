526a434c3e8db9da95f0f1d641398812fea165f9
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-09-06 01:03:39 -0400

    Update descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -4,7 +4,6 @@ export default async function handler(m, { conn, command, args }) {
     await conn.sendMessage(m.chat, { text: 'Ingresa un enlace de Facebook' }, { quoted: m });
     return;
    }
-   m.react('⏳');
    try {
     let api = `https://deliriusapi-official.vercel.app/download/facebook?url=${args[0]}`;
     let response = await axios.get(api);
@@ -13,10 +12,8 @@ export default async function handler(m, { conn, command, args }) {
     if (data.status && url) {
      let res = await axios.get(url, { responseType: 'arraybuffer' });
      await conn.sendMessage(m.chat, { video: res, mimetype: 'video/mp4', caption: data.title }, { quoted: m });
-     m.react('✅');
     } else {
-     await conn.sendMessage(m.chat, { text: 'No se pudo descargar el video, inténtalo de nuevo.' }, { quoted: m });
-     m.react('❎');
+     await conn.sendMessage(m.chat, { text: 'No se pudo descargar el video, inténtalo de nuevo.' }, { quoted: m 
     }
    } catch (e) {
     console.error(e);
