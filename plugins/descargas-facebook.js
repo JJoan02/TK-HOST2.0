e3a0faabb11b470ff30938a80bf9633d4e5b63df
DanıxłJș <116016343+DanixlJs@users.noreply.github.com>
2024-09-06 01:56:09 -0400

    Update descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -8,7 +8,8 @@ export default async function handler(m, { conn, command, args }) {
     let api = `https://deliriusapi-official.vercel.app/download/facebook?url=${args[0]}`;
     let response = await axios.get(api);
     let data = response.data;
-    let url = data.isHdAvailable ? data.urls.find(link => link.hd)?.hd : data.urls.find(link => link.sd)?.sd || undefined;
+    let isHdAvailable = data.isHdAvailable || false;
+    let url = isHdAvailable ? data.urls.find(link => link.hd)?.hd : data.urls.find(link => link.sd)?.sd || undefined;
     if (data.status && url) {
      let res = await axios.get(url, { responseType: 'arraybuffer' });
      await conn.sendMessage(m.chat, { video: res.data, mimetype: 'video/mp4', caption: data.title }, { quoted: m });
