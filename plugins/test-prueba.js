d07f88417e878cf8c81a9e9158ddf5c5adbed2c3
Angelito-OFC <153532713+Angelito-OFC@users.noreply.github.com>
2024-11-20 09:47:30 -0400

    Actualizar plugins

---
+++ plugins/test-prueba.js
@@ -0,0 +1,32 @@
+import https from "https"
+import { sticker } from "../lib/sticker.js"
+
+let handler = async (m, { conn, text }) => {
+    if (!text) throw `Teksnya mana?`    
+    try {
+        let buffer = await IBuffer(`https://api.zenkey.my.id/api/maker/brat?text=${encodeURIComponent(text)}&apikey=zenkey`)
+        let stiker = await sticker(buffer, false, global.packname, global.author)        
+        if (stiker) {
+            await conn.sendFile(m.chat, stiker, 'brat.webp', '', m)
+        }
+    } catch (e) {
+        throw e
+    }
+}
+
+handler.help = ["brat"]
+handler.tags = ["sticker"]
+handler.command = /^(brat)$/i
+
+export default handler
+
+async function IBuffer(url) {
+    return new Promise((resolve, reject) => {
+        https.get(url, (res) => {
+            let data = []
+            res.on('data', chunk => data.push(chunk))
+            res.on('end', () => resolve(Buffer.concat(data)))
+            res.on('error', reject)
+        })
+    })
+}
