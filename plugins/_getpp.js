9cf30ef92b567f40ac9be3700e4f6436ee9f40db
Joan TK <149820143+JJoan02@users.noreply.github.com>
2024-11-25 12:45:20 -0500

    Update _getpp.js

--- plugins/_getpp.js
+++ plugins/_getpp.js
@@ -3,14 +3,21 @@ import fetch from 'node-fetch'
 let handler = async (m, { conn, command }) => {
     try {
         let who
-        if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
-        else who = m.quoted.sender ? m.quoted.sender : m.sender
+        if (m.isGroup) {
+            who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : m.sender
+        } else {
+            who = m.quoted ? m.quoted.sender : m.sender
+        }
+        
         let pp = await conn.profilePictureUrl(who, 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
-        conn.sendFile(m.chat, pp, "nih bang.png", '✧ Listo....', m, { jpegThumbnail: await (await fetch(pp)).buffer() })
-    } catch {
+        let tag = `@${who.replace(/@.+/, '')}`
+        let message = `✧ Aquí tienes la foto de perfil de ${tag} ✧`
+        await conn.sendFile(m.chat, pp, "profile_pic.jpg", message, m, { jpegThumbnail: await (await fetch(pp)).buffer(), mentions: [who] })
+    } catch (e) {
+        console.error('❌ Error al obtener la foto de perfil:', e)
         let sender = m.sender
         let pp = await conn.profilePictureUrl(sender, 'image').catch((_) => "https://telegra.ph/file/24fa902ead26340f3df2c.png")
-        conn.sendFile(m.chat, pp, 'ppsad.png', "✧ Listo....", m, { jpegThumbnail: await (await fetch(pp)).buffer() })
+        await conn.sendFile(m.chat, pp, 'profile_pic_error.jpg', "✧ No se pudo obtener la foto de perfil mencionada, aquí está tu foto de perfil ✧", m, { jpegThumbnail: await (await fetch(pp)).buffer() })
     }
 }
 handler.help = ['getpp <@tag/msj>']
