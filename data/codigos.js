b6bafffc686a4e93b310e42639d2f310a58b4239
Joan TK <149820143+JJoan02@users.noreply.github.com>
2024-11-25 00:49:12 -0500

    Create codigos.js

---
+++ data/codigos.js
@@ -0,0 +1,10 @@
+// database.js
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+export async function openDb() {
+    return open({
+        filename: './data/bot.db',
+        driver: sqlite3.Database
+    });
+}
