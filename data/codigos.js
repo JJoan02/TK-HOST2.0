f003d9432d0fde9e3e8421b42894141910943d26
Joan TK <149820143+JJoan02@users.noreply.github.com>
2024-11-26 10:52:43 -0500

    Update codigos.js

--- data/codigos.js
+++ data/codigos.js
@@ -1,3 +1,4 @@
+// data/codigos.js
 import sqlite3 from 'sqlite3';
 import { open } from 'sqlite';
 import fs from 'fs';
@@ -13,6 +14,7 @@ export async function openDb() {
         // Abrir la conexión a la base de datos
         const db = await open({
             filename: './data/bot.db', // Archivo de base de datos
+            driver: sqlite3.Database // Especifica el driver aquí
         });
         return db;
     } catch (error) {
@@ -20,4 +22,3 @@ export async function openDb() {
         throw error; // Propaga el error para que se gestione más arriba
     }
 }
-await openDb();
