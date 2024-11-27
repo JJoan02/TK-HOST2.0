e8b63a5c82dd8e9bb21c02b886600ddd84fc384b
Joan TK <149820143+JJoan02@users.noreply.github.com>
2024-11-26 19:09:23 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,24 +1,25 @@
 import fetch from 'node-fetch';
 
 export async function handleWelcome(m, { conn, groupMetadata }) {
-  if (!m.messageStubType || m.messageStubType !== 27 || !m.isGroup) return; // Verifica si es un evento válido de bienvenida
+  // Verifica si es un evento válido de bienvenida
+  if (!m.action || m.action !== 'add' || !m.isGroup) return;
 
   try {
     // Obtener imagen de perfil del usuario que se une
-    const pp = await conn.profilePictureUrl(m.messageStubParameters[0], 'image').catch(
+    const pp = await conn.profilePictureUrl(m.participants[0], 'image').catch(
       () => 'https://pomf2.lain.la/f/b03w5p5.jpg' // Imagen por defecto si falla
     );
     const img = await (await fetch(pp)).buffer();
 
     // Verificar si la funcionalidad de bienvenida está habilitada
-    const chat = global.db.data.chats[m.chat];
+    const chat = global.db.data.chats[m.id];
     if (!chat?.bienvenida) return;
 
     // Mensaje de bienvenida personalizado
-    const welcomeMessage = `*⭒─ׄ─ׅ─ׄ─⭒ \`BIENVENIDO\` ⭒─ׄ─ׅ─ׄ─⭒*\n\n╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*\n┊:⁖֟⊱┈֟፝❥ *WELCOME* :: @${m.messageStubParameters[0].split`@`[0]}\n┊:⁖֟⊱┈֟፝❥  ${groupMetadata.subject}\n╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩`;
+    const welcomeMessage = `*⭒─ׄ─ׅ─ׄ─⭒ \`BIENVENIDO\` ⭒─ׄ─ׅ─ׄ─⭒*\n\n╭── ︿︿︿︿︿ *⭒   ⭒   ⭒   ⭒   ⭒   ⭒*\n┊:⁖֟⊱┈֟፝❥ *WELCOME* :: @${m.participants[0].split`@`[0]}\n┊:⁖֟⊱┈֟፝❥  ${groupMetadata.subject}\n╰─── ︶︶︶︶ ✰⃕  ⌇ *⭒ ⭒ ⭒*   ˚̩̥̩̥*̩̩͙✩`;
 
     // Enviar mensaje de bienvenida al grupo
-    await conn.sendMessage(m.chat, { text: welcomeMessage, image: img });
+    await conn.sendMessage(m.id, { text: welcomeMessage, image: img }, { mentions: [m.participants[0]] });
   } catch (err) {
     console.error('Error en handleWelcome:', err);
   }
