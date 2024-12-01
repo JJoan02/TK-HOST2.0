ab1d5e46f2958c1d86912eca29f5ac6bbd669922
Joan TK <149820143+JJoan02@users.noreply.github.com>
2024-12-01 10:18:32 -0500

    Update downloader-play.js

--- plugins/downloader-play.js
+++ plugins/downloader-play.js
@@ -1,84 +1,41 @@
-import yts from 'yt-search'
-import fs from 'fs'
-import os from 'os'
-import axios from 'axios'
+import axios from "axios";
+import ffmpeg from "fluent-ffmpeg"
+import yts from "yt-search"
+import _ from "lodash"
+import fetch from "node-fetch"
 
-const handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw m.reply(`✧ Ejemplo de uso: ${usedPrefix}${command} Joji - Ew`);
-
-  const search = await yts(text);
-  const vid = search.videos[0];
-  if (!vid) throw m.reply('Data no encontrada, intenta con otro titulo');
-
-  const { title, thumbnail, timestamp, views, ago, url } = vid;
-
-await conn.sendMessage(m.chat, { react: { text: '🕒', key: m.key }})
-//  await conn.sendMessage(m.chat, { image: { url: thumbnail }, caption: wait }, { quoted: m });
-
-  try {
-    const response = await axios.get(`https://api.ryzendesu.vip/api/downloader/ytmp3?url=${encodeURIComponent(url)}`);
-    const downloadUrl = response.data.url;
-
-    if (!downloadUrl) throw new Error('Audio URL not found');
-
-    const tmpDir = os.tmpdir();
-    const filePath = `${tmpDir}/${title}.mp3`;
-
-    const audioResponse = await axios({
-      method: 'get',
-      url: downloadUrl,
-      responseType: 'stream',
-    });
-
-    const writableStream = fs.createWriteStream(filePath);
-    audioResponse.data.pipe(writableStream);
-
-    writableStream.on('finish', async () => {
-      await conn.sendMessage(m.chat, {
-        audio: {
-          url: filePath
-        },
-        mimetype: 'audio/mpeg',
-        fileName: `${title}.mp3`,
-        caption: `Titilo: ${title}\nPublicado: ${ago}`,
-        contextInfo: {
-          externalAdReply: {
-            showAdAttribution: true,
-            mediaType: 2,
-            mediaUrl: url,
-            title: title,
-            body: 'Audio Download',
-            sourceUrl: url,
-            thumbnail: await (await conn.getFile(thumbnail)).data,
-          },
-        },
-      }, { quoted: m });
-await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key }})
-
-      fs.unlink(filePath, (err) => {
-        if (err) {
-          console.error(`Failed to delete audio file: ${err}`);
-        } else {
-          console.log(`Deleted audio file: ${filePath}`);
-        }
-      });
-    });
-
-    writableStream.on('error', (err) => {
-      console.error(`Failed to write audio file: ${err}`);
-      m.reply('Failed to download audio');
-    });
-  } catch (error) {
-    console.error('Error:', error.message);
-    throw `Error: ${error.message}. Please check the URL and try again.`;
-  }
-};
-
-handler.help = ['play'].map((v) => v + ' *<consulta>*');
-handler.tags = ['downloader'];
-handler.command = /^(play)$/i;
-
-handler.register = true
-handler.disable = false
+let handler = async (m, { conn, args, usedPrefix, command, text }) => {
+if (!text || text.trim() === "") return m.reply(`Ejemplo: ${usedPrefix + command} 505`);
+m.reply(wait);
+try {
+async function getBuffer(url) {
+const res = await axios({
+method: 'get',
+url,
+responseType: 'arraybuffer'
+});
+return res.data;
+}
+const res = await axios.get(`https://Ikygantengbangetanjay-api.hf.space/yt?query=${encodeURIComponent(text)}`);
+const video = res.data.result;
+if (!video) return m.reply('Video/Audio no encontrado');
+if (video.duration.seconds >= 3600) {
+return reply('El audio es demasiado largo!');
+}
+const audioUrl = video.download.audio;
+const videoUrl = video.download.video;
+if (!audioUrl || !videoUrl) {
+return reply("No se pudo obtener la URL de audio/vídeo. Por favor inténtalo de nuevo.");
+}
+const thumbBuffer = await getBuffer(video.thumbnail);
+//await conn.sendMessage(m.chat, { video: { url: videoUrl }, mimetype: 'video/mp4', fileName: `${video.title}.mp4`, jpegThumbnail: thumbBuffer, caption: `🎥 *${video.title}*\n📽 *Fuente*: ${video.url}` }, { quoted: m });
+await conn.sendMessage(m.chat, { audio: { url: audioUrl }, mimetype: 'audio/mpeg', fileName: `${video.title}.mp3`, jpegThumbnail: thumbBuffer }, { quoted: m });
+} catch (e) {
+m.reply(`*Error:* ${e.message}`);
+}
+}
+handler.help = ["play *<consulta>*"]
+handler.tags = ["downloader"]
+handler.command = /^(play|ytplay)$/i
 
 export default handler
