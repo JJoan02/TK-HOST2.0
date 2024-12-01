56b58648dc3c0510c54a98601d8fbae0fe197385
Joan TK <149820143+JJoan02@users.noreply.github.com>
2024-12-01 11:41:31 -0500

    Update downloader-play2.js

--- plugins/downloader-play2.js
+++ plugins/downloader-play2.js
@@ -16,25 +16,23 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
       return await handleNoResults(m, conn);
     }
 
-    await updateVideoInfo(m, conn, initialMessage, videoData, '🕒 *Preparando descarga del video...*');
+    await updateVideoInfo(m, conn, initialMessage, videoData);
     const { videoUrl, audioUrl, thumbBuffer } = await downloadMediaWithQualityControl(videoData.url, text);
     if (!videoUrl) {
       return await handleDownloadError(m, conn, 'No se pudo obtener la URL del video. Por favor inténtalo de nuevo.');
     }
 
     // Enviar el video primero
+    await updateDownloadStatus(m, conn, initialMessage, videoData, 'Video descargado...');
     await sendVideoFile(m, conn, videoData, videoUrl, thumbBuffer);
-    await updateVideoInfo(m, conn, initialMessage, videoData, '🎬 *Video descargado...*
-🎶 *Preparando descarga de audio*');
 
     if (!audioUrl) {
       return await handleDownloadError(m, conn, 'No se pudo obtener la URL del audio. Por favor inténtalo de nuevo.');
     }
 
     // Enviar el audio después
+    await updateDownloadStatus(m, conn, initialMessage, videoData, 'Audio descargado...');
     await sendAudioFile(m, conn, videoData, audioUrl, thumbBuffer);
-    await updateVideoInfo(m, conn, initialMessage, videoData, '🎬 *Video descargado...*
-🎶 *Audio descargado...*');
   } catch (error) {
     console.error('Error en el proceso:', error);
     await handleUnexpectedError(m, conn);
@@ -76,7 +74,7 @@ async function handleUnexpectedError(m, conn) {
 // Enviar mensaje inicial indicando que se está procesando
 async function sendInitialMessage(m, conn) {
   let initialMessage = await conn.sendMessage(m.chat, {
-    text: '✧ Procesando solicitud un momento...'
+    text: '✧ Espere un momento...'
   }, { quoted: m });
   await conn.sendMessage(m.chat, { react: { text: '⏳', key: m.key } });
   return initialMessage;
@@ -89,7 +87,7 @@ async function searchVideo(query) {
 }
 
 // Editar mensaje con la información del video
-async function updateVideoInfo(m, conn, initialMessage, videoData, status) {
+async function updateVideoInfo(m, conn, initialMessage, videoData) {
   await conn.sendMessage(m.chat, {
     text: `🔰 *Admin-TK Downloader*
 
@@ -99,7 +97,7 @@ async function updateVideoInfo(m, conn, initialMessage, videoData, status) {
 📅 *Publicado:* ${videoData.ago}
 🌐 *Enlace:* ${videoData.url}
 
-${status}`,
+🕒 *Preparando descarga...*`,
     edit: initialMessage.key
   });
 }
@@ -136,6 +134,23 @@ async function getBuffer(url) {
   return res.data;
 }
 
+// Editar mensaje indicando que el medio ha sido descargado
+async function updateDownloadStatus(m, conn, initialMessage, videoData, status) {
+  await conn.sendMessage(m.chat, {
+    text: `🔰 *Admin-TK Downloader*
+
+🎵 *Título:* ${videoData.title}
+⏳ *Duración:* ${videoData.duration.timestamp}
+👁️ *Vistas:* ${videoData.views}
+📅 *Publicado:* ${videoData.ago}
+🌐 *Enlace:* ${videoData.url}
+
+🕒 *${status}*`,
+    edit: initialMessage.key
+  });
+  await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } });
+}
+
 // Enviar el archivo de audio descargado
 async function sendAudioFile(m, conn, videoData, downloadUrl, thumbBuffer) {
   const doc = {
@@ -155,6 +170,7 @@ async function sendAudioFile(m, conn, videoData, downloadUrl, thumbBuffer) {
     }
   };
   await conn.sendMessage(m.chat, doc, { quoted: m });
+  await conn.sendMessage(m.chat, { text: '⚠️ *Admin-TK:* El archivo ha sido enviado exitosamente. Si necesitas algo más, no dudes en pedírmelo.', quoted: m });
 }
 
 // Enviar archivo de video descargado
@@ -168,6 +184,8 @@ async function sendVideoFile(m, conn, videoData, videoUrl, thumbBuffer) {
 📽 *Fuente*: ${videoData.url}`
   };
   await conn.sendMessage(m.chat, videoDoc, { quoted: m });
+  await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } });
+  await conn.sendMessage(m.chat, { text: '⚠️ *Admin-TK:* El video ha sido enviado exitosamente. Si necesitas algo más, no dudes en pedírmelo.', quoted: m });
 }
 
 handler.help = ['play2 *<consulta>*', 'playvideo *<consulta>*'];
@@ -175,4 +193,3 @@ handler.tags = ['downloader'];
 handler.command = /^(play2|playvideo)$/i;
 
 export default handler;
-        
