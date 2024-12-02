318f14f205f13cd05849b6fa5469df341ce650e1
Joan TK <149820143+JJoan02@users.noreply.github.com>
2024-12-02 17:53:02 -0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -10,15 +10,13 @@ const {
     getContentType 
 } = (await import('@adiwajshing/baileys')).default
 
-process.env.TZ = 'America/Lima'
+process.env.TZ = 'America/Buenos_Aires'
 import fs from "fs";
 import path from "path";
 import fetch from "node-fetch";
 import moment from "moment-timezone";
-import { xpRange } from '../lib/levelling.js';
-
-// Listado de categorías del menú
-const arrayMenu = [
+import { xpRange } from '../lib/levelling.js'
+let arrayMenu = [
     'all',
     'main',
     'anonymous',
@@ -36,90 +34,92 @@ const arrayMenu = [
     'tools',
     'group',
     'owner',
-    ''
+''
 ];
 
-// Función para estilizar el texto
-const estilo = (text, style = 1) => {
-    const xStr = 'abcdefghijklmnopqrstuvwxyz1234567890'.split('');
-    const yStr = Object.freeze({
-        1: 'ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘqʀꜱᴛᴜᴠᴡxʏᴢ1234567890'
-    });
-    const replacer = xStr.map((v, i) => ({ original: v, convert: yStr[style].split('')[i] }));
-    return text.toLowerCase().split('').map(v => replacer.find(x => x.original == v)?.convert || v).join('');
+let estilo = (text, style = 1) => {
+  var xStr = 'abcdefghijklmnopqrstuvwxyz1234567890'.split('');
+  var yStr = Object.freeze({
+    1: 'ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘqʀꜱᴛᴜᴠᴡxʏᴢ1234567890'
+  });
+  var replacer = [];
+  xStr.map((v, i) => replacer.push({
+    original: v,
+    convert: yStr[style].split('')[i]
+  }));
+  var str = text.toLowerCase().split('');
+  var output = [];
+  str.map(v => {
+    const find = replacer.find(x => x.original == v);
+    find ? output.push(find.convert) : output.push(v);
+  });
+  return output.join('');
 };
 
-// Definición de categorías del menú con emogis y descripción
 const allTags = {
-    all: "🏠 *MENU COMPLETO*",
-    main: "📜 *MENU PRINCIPAL* - Accede a las funciones principales del bot",
-    downloader: "📀 *MENU DESCARGAS* - Descarga videos, música y más",
-    jadibot: "🤖 *MENU SUBBOTS* - Convierte tu número en un subbot",
-    rpg: "🌟 *MENU RPG* - Juega al RPG y sube de nivel",
-    ai: "🤖 *MENU IA* - Accede a funciones de inteligencia artificial",
-    search: "🔍 *MENU BÚSQUEDA* - Encuentra información rápidamente",
-    anime: "🌸 *MENU ANIME* - Comandos relacionados con anime y manga",
-    sticker: "🎟️ *MENU STICKERS* - Crea y envía stickers personalizados",
-    fun: "🎉 *MENU DIVERSION* - Comandos de entretenimiento y juegos",
-    group: "👥 *MENU GRUPO* - Administración de grupos de WhatsApp",
-    nsfw: "😈 *MENU NSFW* - Contenido solo para adultos",
-    info: "📃 *MENU INFORMACIÓN* - Información sobre el bot y otros datos",
-    internet: "🌐 *MENU INTERNET* - Herramientas relacionadas con Internet",
-    owner: "👑 *MENU ADMIN* - Funciones solo para el propietario del bot",
-    tools: "🤨 *MENU HERRAMIENTAS* - Herramientas útiles para diferentes tareas",
-    anonymous: "👻 *CHAT ANÓNIMO* - Chatea de manera anónima",
-    "": "💡 *SIN CATEGORÍA*"
+  all: "MENU COMPLETO",
+  main: "MENU PRINCIPAL",
+  downloader: "MENU DOWNLOADER",
+  jadibot: "MENU SUBBOTS",
+  rpg: "MENU RPG",
+  ai: "MENU IA",
+  search: "MENU BUSQUEDA",
+  anime: "MENU ANIME",
+  sticker: "MENU STICKER",
+  fun: "MENU FUN",
+  group: "MENU GRUPO",
+  nsfw: "MENU NSFW",
+  info: "MENU INFO",
+  internet: "MENU INTERNET",
+  owner: "MENU OWNER",
+  tools: "MENU TOOLS",
+  anonymous: "ANONYMOUS CHAT",
+  "": "NO CATEGORY"
 }
 
-// Plantilla por defecto del menú
 const defaultMenu = {
     before: `
-*Hola %name* 👋
-Soy *Admin-TK*, un bot de WhatsApp para ayudarte a realizar diferentes tareas, buscar información y mucho más.
+Hola %name
+Soy un sistema automatizado (WhatsApp Bot) que puede ayudar a hacer algo, buscar y obtener datos/información sólo a través de WhatsApp.
 
-*Detalles del Sistema:*
-- Librería: *Baileys*
-- Funcionalidad: *Asistente*
+◦ *Libreria:* Baileys
+◦ *Funcion:* Assistant
 
-➜ *Tiempo Activo*: %uptime
-➜ *Fecha*: %date
-➜ *Hora*: %time
-➜ *Prefijo Usado*: [ %p ]
+┌  ◦ Rutina : %uptime
+│  ◦ Fecha : %date
+│  ◦ Hora : %time
+└  ◦ Prefijo Usado : *[ %p ]*
 `.trimStart(),
-    header: '🔗 *%category*',
-    body: '├ 📌 %cmd %islimit %isPremium',
-    footer: '└───────
-',
-    after: `
-*Nota:* Puedes escribir .menu <categoría> para ver un menú específico.
-★ Ejemplo: *.menu tools*
-`
+    header: '┌  ◦ *%category*',
+    body: '│  ◦ %cmd %islimit %isPremium',
+    footer: '└  ',
+    after: `*Nota:* Escribe .menu <categoría> para seleccionar un menu en específico\n✧ Ejemplo: .menu tools`
 }
 
 let handler = async (m, { conn, usedPrefix: _p, args = [], command }) => {
     try {
-        let { exp, limit, level, role } = global.db.data.users[m.sender];
-        let { min, xp, max } = xpRange(level, global.multiplier);
-        let name = `@${m.sender.split`@`[0]}`;
-        let teks = args[0] || '';
+//        let package = JSON.parse(await fs.promises.readFile(path.join(__dirname, '../package.json')).catch(_ => '{}'))
+        let { exp, limit, level, role } = global.db.data.users[m.sender]
+        let { min, xp, max } = xpRange(level, global.multiplier)
+        let name = `@${m.sender.split`@`[0]}`
+        let teks = args[0] || ''
         
-        // Obtener fecha y hora local
-        let d = new Date(new Date() + 3600000);
-        let locale = 'es';
+        let d = new Date(new Date + 3600000)
+        let locale = 'es'
         let date = d.toLocaleDateString(locale, {
             day: 'numeric',
             month: 'long',
             year: 'numeric'
-        });
+        })
         
         let time = d.toLocaleTimeString(locale, {
             hour: 'numeric',
             minute: 'numeric',
             second: 'numeric'
-        });
+        })
 
-        let _uptime = process.uptime() * 1000;
-        let uptime = clockString(_uptime);
+        let _uptime = process.uptime() * 1000
+        let uptime = clockString(_uptime)
         
         let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
             return {
@@ -129,18 +129,17 @@ let handler = async (m, { conn, usedPrefix: _p, args = [], command }) => {
                 limit: plugin.limit,
                 premium: plugin.premium,
                 enabled: !plugin.disabled,
-            };
-        });
+            }
+        })
 
-        // Mostrar el menú principal si no se pasa un argumento específico
         if (!teks) {
-            let menuList = `${defaultMenu.before}\n\n┌─── 🏠 *LISTA DE MENUS*\n`;
+            let menuList = `${defaultMenu.before}\n\n┌  ◦ *LISTA DE MENUS*\n`
             for (let tag of arrayMenu) {
                 if (tag && allTags[tag]) {
-                    menuList += `├ 👉 ${_p}menu ${tag}\n`;
+                    menuList += `│  ◦ ${_p}menu ${tag}\n`
                 }
             }
-            menuList += `└───────\n\n${defaultMenu.after}`;
+            menuList += `└  \n\n${defaultMenu.after}`
 
             let replace = {
                 '%': '%',
@@ -149,58 +148,73 @@ let handler = async (m, { conn, usedPrefix: _p, args = [], command }) => {
                 name, 
                 date,
                 time
-            };
+            }
 
             let text = menuList.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), 
-                (_, name) => '' + replace[name]);
+                (_, name) => '' + replace[name])
 
-            await conn.sendFile(m.chat, "https://pomf2.lain.la/f/ucogaqax.jpg", 'menu.jpg', estilo(text), global.fliveLoc2, null);
-            return;
+conn.sendFile(m.chat, "https://pomf2.lain.la/f/4gv01t8y.png", 'menu.jpg', estilo(text), global.fliveLoc2, null)
+
+/*            await conn.relayMessage(m.chat, {
+            extendedTextMessage:{
+                text: text, 
+                contextInfo: {
+                    mentionedJid: [m.sender],
+                    externalAdReply: {
+                        title: date,
+                        mediaType: 1,
+                        previewType: 0,
+                        renderLargerThumbnail: true,
+                        thumbnailUrl: 'https://pomf2.lain.la/f/4gv01t8y.png',
+                        sourceUrl: 'https://whatsapp.com/channel/0029VarbyoN2ZjCkcPW7q33F'
+                    }
+                }, 
+                mentions: [m.sender]
+            }
+        }, {})*/
+            return
         }
 
-        // Mostrar menú específico
         if (!allTags[teks]) {
-            return m.reply(`El menú "${teks}" no está registrado.
-Escribe ${_p}menu para ver la lista de menús.`);
+            return m.reply(`El menu "${teks}" no está registrado.\nEscribe ${_p}menu para ver la lista de menus.`)
         }
 
-        let menuCategory = defaultMenu.before + '\n\n';
+        let menuCategory = defaultMenu.before + '\n\n'
         
         if (teks === 'all') {
-            // Mostrar todos los menús
+            // category all
             for (let tag of arrayMenu) {
                 if (tag !== 'all' && allTags[tag]) {
-                    menuCategory += defaultMenu.header.replace(/%category/g, allTags[tag]) + '\n';
+                    menuCategory += defaultMenu.header.replace(/%category/g, allTags[tag]) + '\n'
                     
-                    let categoryCommands = help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help);
+                    let categoryCommands = help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help)
                     for (let menu of categoryCommands) {
                         for (let help of menu.help) {
                             menuCategory += defaultMenu.body
                                 .replace(/%cmd/g, menu.prefix ? help : _p + help)
                                 .replace(/%islimit/g, menu.limit ? '(Ⓛ)' : '')
-                                .replace(/%isPremium/g, menu.premium ? '(Ⓟ)' : '') + '\n';
+                                .replace(/%isPremium/g, menu.premium ? '(Ⓟ)' : '') + '\n'
                         }
                     }
-                    menuCategory += defaultMenu.footer + '\n';
+                    menuCategory += defaultMenu.footer + '\n'
                 }
             }
         } else {
-            // Mostrar menú específico
-            menuCategory += defaultMenu.header.replace(/%category/g, allTags[teks]) + '\n';
+            menuCategory += defaultMenu.header.replace(/%category/g, allTags[teks]) + '\n'
             
-            let categoryCommands = help.filter(menu => menu.tags && menu.tags.includes(teks) && menu.help);
+            let categoryCommands = help.filter(menu => menu.tags && menu.tags.includes(teks) && menu.help)
             for (let menu of categoryCommands) {
                 for (let help of menu.help) {
                     menuCategory += defaultMenu.body
                         .replace(/%cmd/g, menu.prefix ? help : _p + help)
                         .replace(/%islimit/g, menu.limit ? '(Ⓛ)' : '')
-                        .replace(/%isPremium/g, menu.premium ? '(Ⓟ)' : '') + '\n';
+                        .replace(/%isPremium/g, menu.premium ? '(Ⓟ)' : '') + '\n'
                 }
             }
-            menuCategory += defaultMenu.footer + '\n';
+            menuCategory += defaultMenu.footer + '\n'
         }
 
-        menuCategory += '\n' + defaultMenu.after;
+        menuCategory += '\n' + defaultMenu.after
         
         let replace = {
             '%': '%',
@@ -209,30 +223,47 @@ Escribe ${_p}menu para ver la lista de menús.`);
             name,
             date,
             time
-        };
+        }
 
         let text = menuCategory.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), 
-            (_, name) => '' + replace[name]);
+            (_, name) => '' + replace[name])
 
-        await conn.sendFile(m.chat, "https://pomf2.lain.la/f/ucogaqax.jpg", 'menu.jpg', estilo(text), global.fliveLoc2, null);
+conn.sendFile(m.chat, "https://pomf2.lain.la/f/7b5qzd8.png", 'menu.jpg', estilo(text), global.fliveLoc2, null)
+
+/*        await conn.relayMessage(m.chat, {
+            extendedTextMessage:{
+                text: text, 
+                contextInfo: {
+                    mentionedJid: [m.sender],
+                    externalAdReply: {
+                        title: date,
+                        mediaType: 1,
+                        previewType: 0,
+                        renderLargerThumbnail: true,
+                        thumbnailUrl: 'https://pomf2.lain.la/f/7b5qzd8.png',
+                        sourceUrl: 'https://whatsapp.com/channel/0029VarbyoN2ZjCkcPW7q33F'
+                    }
+                }, 
+                mentions: [m.sender]
+            }
+        }, {})*/
     } catch (e) {
-        conn.reply(m.chat, 'Perdón, hubo un error al mostrar el menú.', m);
-        console.error(e);
+        conn.reply(m.chat, 'Perdon, hay un error con el menu', m)
+        console.error(e)
     }
-};
+}
 
-handler.help = ['menu'];
-handler.tags = ['main'];
-handler.command = /^(menu|help)$/i;
-handler.exp = 3;
+handler.help = ['menu']
+handler.tags = ['main']
+handler.command = /^(menu|help)$/i
+handler.exp = 3
 
 export default handler;
 
-// Función para dar formato al tiempo de actividad
 function clockString(ms) {
-    if (isNaN(ms)) return '--';
-    let h = Math.floor(ms / 3600000);
-    let m = Math.floor(ms / 60000) % 60;
-    let s = Math.floor(ms / 1000) % 60;
-    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':');
-    }
+    if (isNaN(ms)) return '--'
+    let h = Math.floor(ms / 3600000)
+    let m = Math.floor(ms / 60000) % 60
+    let s = Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+            }
